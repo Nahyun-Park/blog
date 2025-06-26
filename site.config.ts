898cdc1f4136c0a2f053c74f90958453e039f42b
nahyun-dotcom <nahyunpark51@gmail.com>
2025-06-26 16:27:01 +0900

    Update to new Notion page with Welcome to Notion

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '21ebe77da59280b18131fa4cccd1d96f',
+  rootNotionPageId: '21e98e2c9a43805780b8c9d42871ee10',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
