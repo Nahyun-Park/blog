196bc4a0e30df50190464faaa367d9b0fe5a4eeb
nahyun-dotcom <nahyunpark51@gmail.com>
2025-06-26 15:45:42 +0900

    Update Notion page ID to new blog page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '21eadc32945b800a9d65e251f3b223c9',
+  rootNotionPageId: '21ebe77da59280b18131fa4cccd1d96f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
